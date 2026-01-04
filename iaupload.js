8a1c80c99e0712c45b0b32cff87660fae6868e79
Ong Kai Jin <47915713+OngKaiJin@users.noreply.github.com>
2026-01-04 23:36:41 +0800

    Update iaupload.js

--- iaupload.js
+++ iaupload.js
@@ -1,9 +1,11 @@
 import fs from "fs/promises";
 import crypto from "crypto";
 import {fileTypeFromFile} from "file-type";
+import he from "he";
 
 let nodes = JSON.parse(await fs.readFile('../getEpisodes 02-01-2026.json', 'utf-8'));
 let priority = [11525];
+let addtitle = "\nfunction fill (name, title) {document.querySelector('[name=\"' + document.querySelector('[value=\"' + name + '\"]').name.replace(\"filename\", \"\") + \"title\" + '\"]').value = title;}";
 let prioritynodes = nodes.filter(i => {
 	for (let x of priority) {
 		if (x === i.id) {
@@ -27,11 +29,15 @@ async function generate () {
 				let mfilename = '';
 				if (filename.substring(filename.lastIndexOf(".") + 1) !== filetype.ext) {
 					mfilename = ' --remote-name "' + filename.substring(0, filename.lastIndexOf(".") + 1) + filetype.ext + '"';
+					addtitle += '\nfill("' + filename.substring(0, filename.lastIndexOf(".") + 1) + filetype.ext + '","' + he.decode(j.title) + '");';
+				} else {
+					addtitle += '\nfill("' + filename + '","' + he.decode(j.title) + '");';
 				}
 				console.log('ia up fmfenxiang-' + i.id + ' "'+ j.id + "/" + filename + '"' + mfilename + info);
 				info = '';
 			}
 		}
 	}
+	console.log(addtitle);
 }
 generate();
