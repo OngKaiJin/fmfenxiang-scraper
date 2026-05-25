841c8cf9922810fc1dc2a2fb144434ba9611e6b7
Ong Kai Jin <47915713+OngKaiJin@users.noreply.github.com>
2026-05-26 01:41:01 +0800

    Update fmm.js
    Interface slightly changed; renewing cookie, item 13221

--- fmm.js
+++ fmm.js
@@ -2,12 +2,12 @@ import fs from "fs/promises";
 import dom from "jsdom";
 import crypto from "crypto";
 
-let cookie_ongkaijin777 = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6InZOVGFYbGcrdk93bUxSS2Flakg3REE9PSIsInZhbHVlIjoieW5Od29NVGtKNk8wWEh0OUlVeExOL3A5Q0ZabFMzOUJVdnNsQXBRUDd0MGUrMGZyNUx1dGVLUTYwa2k3U25NTEFjbHhwN0I2aE1vU2xoaVdxRG9BZ0NYYkwxbzIrdkJNZ2JHRnYrZllkZUQyWm01ekNLclpaczJHeC9VVzhaWDZ1K24yTEVnbTlNREpBcmlaZ0lxeE4wZzVqQzhmcEkrbVdES3RPYm9yeWJZN2JsWENWUDg4cVZMenFzL29meExBSVRxT1FDc1RNQVNRUnZRSzBhZmpqMk9vbS82dEYrU2k2UXAvZnA5a3I4dz0iLCJtYWMiOiJlZGUzZWQ0Nzg1NWZkM2JhZmIzNDcyMGVjNmUzMDVkMDc0YzZlNmU3ZjA1Y2Q1NGQ5ZjVkZWZjNWU0NDVmYTlkIiwidGFnIjoiIn0%3D";
+let cookie_ongkaijin777 = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IjN0U1NpSHlXbmZXZ1lxMFNtdFpGYVE9PSIsInZhbHVlIjoiVHJDSnROQmQ4Sm5uZTVPRTczYisvSFVCSTlwMXBFekUySkNRUVJTUWY5U1JhREtXbnhsLzJjeXlzWkozUmE2MnBhdTFBVitFR1gzSzJDSFFxb1FNRmdyWjdkaTY3NDVSdTdKWXMyOUMvSy9lbkFlNVdkeGhWMDJtejU0RmhxY2YrMFMxaWwveGRDOFliem9rZmxqbmxjd1V2djZ2NHA2SU95S0ZDTXpUQlk4YitnZlBsdWVJSzdlWlZnTFVqTkorTisxUUQwNy9ENmkzODJzOTRMNGN1YlJPZU5sS0ZuWlkwVjJSK3hseHp0VT0iLCJtYWMiOiI4OTY4ZjViNThiZTdlZTRlM2MxZGM2M2I4MDFhZGIxNDkxOGJhYzM5ZmYwZmUyNzI0NzAzNzk5NjNmNmNlYmM4IiwidGFnIjoiIn0%3D";
 let cookie_lilychu4566 = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IjFjQlQ0NzNMdWdKUTFkMDlKODJuUWc9PSIsInZhbHVlIjoiVjlPNlIySURMbUlaYmVWWGJ1a2x3Y3hZdVU3OUNDbFlUQ3JpUU5zZmQrY0FiSXRCaDBsTTkvOFhWcXQvUmJEWDUzajJXTUsvblE4Vk5CMDdpVkNkNU5EOHYzYzdKNjBEd0FoVnc0Y2RpTnlEVjZEVnJEcEtZU0t5THIvS0xybmNwY241elN2YVhiZCtDNjNCcVVFTWR2Wmdldi91UnJTMDdoMmpZa1N3QkpRaWFjdTc2eEtzVXlqRExKYmxBOWpkMHpsOXAwWGtuQ09kWk1PVDQ2SitvbDViSmJGWjNsUEdybWRNRUR6VlNPWT0iLCJtYWMiOiJhZjhhOTg5NzI1OGI4MzZkYzFiM2YwYjRlYTkyOTBlOTQzNDFmNzA0NjA3MWZhMmY2ODhmMzI2NmZiN2I4ZGNhIiwidGFnIjoiIn0%3D";
 let cookie_lilychu1779 = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IjRublg3MW90dGVQN21qR015MlBzeVE9PSIsInZhbHVlIjoicW5Wa2RjeE9yakNNQ29SbW5lZ09rSUFsU2padUNyUnlMT2dUNXRKVE1yWjFmUFFQRGJqZHlJRVhpYXhwaWxNV01VbFc0RVJFbDNYSmRpT0d2MXZwNnFIRTUxeUVPdHJYdkt6TUVmRW5UR1Y5TVU5cUs4TkVRejhRNnIrN1FoM1VmQWxVbFpId1N6VTZkckhGblQ1b1NycnQwSGdDbk5BNUFWbnlQUmlJd3hVNmhjMVozb1RVc2NreDdCRTZmV2JObG9JWEFYS1k5SkxNZ3hOR2dObFVlM2kzbUJlRjgvNlk2VjlJUmFrbTBFaz0iLCJtYWMiOiIxYzU2NjNhNTdmZDU4N2FjMDQzNTRmM2IxNzgxZjIzMjMxYzExMWZlZDczZWIwMTEwNTJjZGJlNDdlYzJlNTFmIiwidGFnIjoiIn0%3D";
 
-let nodes = JSON.parse(await fs.readFile('../getEpisodes 02-01-2026.json', 'utf-8'));
-let priority = [12240, 11376, 11779, 10616, 11438, 12001, 12226, 12341, 12667, 11403, 11942, 12613, 10459, 11470, 10917, 11202, 11767, 12211];
+let nodes = JSON.parse(await fs.readFile('../getEpisodes 13221 25-05-2026.json', 'utf-8'));
+let priority = [];
 let count = [];
 
 async function queue (list) {
@@ -46,7 +46,7 @@ async function checkexist (path, filename) {
 async function loop (start) {
 	let i = start;
 	while (i < count.length) {
-		let page = await fetch("https://fmfenxiang.com/sound/" + count[i].id, {headers: {cookie: cookie_lilychu1779}});
+		let page = await fetch("https://fmfenxiang.com/sound/" + count[i].id, {headers: {cookie: cookie_ongkaijin777}});
 		console.log("---" + (i + 1) + "/" + count.length + "---");
 		console.log(page.status + " " + count[i].id);
 		if (!page.status === 200) {
@@ -64,7 +64,8 @@ async function loop (start) {
 			return loop(i + 1);
 		} else {
 			let doc = new dom.JSDOM(await page.text());
-			let url = doc.window.document.querySelector("audio").src;
+			let player = JSON.parse('"' + doc.window.document.querySelector("#fm-player-col").getAttribute("x-init") + '"').match(/\(([^)]+)\)/)[1].split(',').map((i) => i.trim().replace(/^['"]|['"]$/g, ''));
+			let url = player[0];
 			let filename = decodeURIComponent(url).split('?')[0].split('/').reverse()[0];
 			if (filename.includes("|") || filename.includes("<")) {
 				filename = encodeURIComponent(filename);
@@ -101,7 +102,7 @@ async function loop (start) {
 				await fs.writeFile("../fmfenxiang/" + count[i].id + "/" + filename, await resp.body);
 				await fs.mkdir ("../fmfenxiang-log/" + count[i].id, {recursive: true});
 				await fs.writeFile("../fmfenxiang-log/" + count[i].id + "/" + "dir.txt", filename, 'utf-8');
-				await fs.writeFile("../fmfenxiang-log/" + count[i].id + "/" + filename + ".txt", page.status + " " + resp.status + " " + Date.now() + " " + url + " " + new Uint8Array(await crypto.subtle.digest('SHA-1', await resp2.arrayBuffer())).toHex() + " " + "cookie_lilychu1779", 'utf-8');
+				await fs.writeFile("../fmfenxiang-log/" + count[i].id + "/" + filename + ".txt", page.status + " " + resp.status + " " + Date.now() + " " + url + " " + new Uint8Array(await crypto.subtle.digest('SHA-1', await resp2.arrayBuffer())).toHex() + " " + "cookie_ongkaijin777", 'utf-8');
 				await checkpoint(i);
 			} catch (error) {
 				console.log(error);
